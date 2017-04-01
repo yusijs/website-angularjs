fea8b0aeeb7259bc95a5519837f410c4b02aab6a
Ronnie Laugen <ronnie@debian.workgroup>
2017-04-01 23:06:04 +0200

    clickOutsideToClose to true

--- app/common/footer/footer.controller.js
+++ app/common/footer/footer.controller.js
@@ -16,7 +16,8 @@ class FooterController {
     showPrompt(ev) {
     this.$mdDialog.show({
       contentElement: '#footer-dialog',
-      parent: angular.element(document.body)
+      parent: angular.element(document.body),
+      clickOutsideToClose:true
     })
     .then(details => {
       console.log(details);
